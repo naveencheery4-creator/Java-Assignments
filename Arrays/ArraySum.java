7bbb7ad4ac90b6c9bcdf4c4c5e01e3d4d778d441
naveencheery4-creator <naveencheery4@gmail.com>
2026-06-12 16:15:55 +0530

    Create ArraySum.java

---
+++ Arrays/ArraySum.java
@@ -0,0 +1,19 @@
+public class ArraySum {
+
+    public static void main(String[] args) {
+
+        int[] numbers = {10, 20, 30, 40, 50};
+
+        int sum = 0;
+
+        for (int i = 0; i < numbers.length; i++) {
+            sum = sum + numbers[i];
+        }
+
+        System.out.println("Sum of Array Elements = " + sum);
+    }
+}
+
+/* Output
+  Sum of Array Elements = 150
+  */
