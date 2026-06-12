d999831a6559a6982924c2444f85f522e4063c9d
naveencheery4-creator <naveencheery4@gmail.com>
2026-06-12 14:49:44 +0530

    Create CommentsDemo.java

---
+++ Java-Basics/CommentsDemo.java
@@ -0,0 +1,15 @@
+
+public class CommentsDemo {
+
+    public static void main(String[] args) {
+
+        // This is a Single-line Comment
+        System.out.println("Single-line Comment Example");
+
+        /*
+         * This is a Multi-line Comment
+         * It can span multiple lines
+         */
+        System.out.println("Multi-line Comment Example");
+    }
+}
