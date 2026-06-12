d7d0863e50ef4a54cbb7b675c396e8bd917e85dd
naveencheery4-creator <naveencheery4@gmail.com>
2026-06-12 15:07:17 +0530

    Create SmallerLargerNumber.java

---
+++ Operators/SmallerLargerNumber.java
@@ -0,0 +1,23 @@
+public class SmallerLargerNumber {
+
+    public static void main(String[] args) {
+
+        int num1 = 25;
+        int num2 = 40;
+
+        if (num1 > num2) {
+            System.out.println("Larger Number: " + num1);
+            System.out.println("Smaller Number: " + num2);
+        } else {
+            System.out.println("Larger Number: " + num2);
+            System.out.println("Smaller Number: " + num1);
+        }
+    }
+}
+
+
+/*output
+Larger Number: 40
+Smaller Number: 25
+  */
+  
