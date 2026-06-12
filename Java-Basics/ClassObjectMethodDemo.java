d881c8874eb4e5a2abc6c5378b67cb4ecba37fee
naveencheery4-creator <naveencheery4@gmail.com>
2026-06-12 14:46:53 +0530

    Create ClassObjectMethodDemo.java

---
+++ Java-Basics/ClassObjectMethodDemo.java
@@ -0,0 +1,17 @@
+//class
+public class ClassObjectMethodDemo {
+
+    // Method
+    public void display() {
+        System.out.println("This is a method.");
+    }
+
+    public static void main(String[] args) {
+
+        // Object Creation
+        ClassObjectMethodDemo obj = new ClassObjectMethodDemo();
+
+        // Method Call
+        obj.display();
+    }
+}
