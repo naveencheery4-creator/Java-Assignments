f046bf6a136dd8a65f82455c7a3cd5a146a29299
naveencheery4-creator <naveencheery4@gmail.com>
2026-06-12 15:52:38 +0530

    Create PrimeNumber.java

---
+++ Loops/PrimeNumber.java
@@ -0,0 +1,30 @@
+public class PrimeNumber {
+
+    public static void main(String[] args) {
+
+        int number = 13;
+        boolean isPrime = true;
+
+        if (number <= 1) {
+            isPrime = false;
+        } else {
+
+            for (int i = 2; i < number; i++) {
+
+                if (number % i == 0) {
+                    isPrime = false;
+                    break;
+                }
+            }
+        }
+
+        if (isPrime) {
+            System.out.println(number + " is a Prime Number");
+        } else {
+            System.out.println(number + " is not a Prime Number");
+        }
+    }
+}
+/* Output
+  13 is a Prime Number
+  */
