9b2e50d6309aa7356ad1817bd6855c1d30738750
naveencheery4-creator <naveencheery4@gmail.com>
2026-06-12 15:38:49 +0530

    Create EqualNotEqualInLoops.java

---
+++ Loops/EqualNotEqualInLoops.java
@@ -0,0 +1,21 @@
+public class EqualNotEqualInLoops {
+
+    public static void main(String[] args) {
+
+        int a = 10;
+        int b = 20;
+
+        System.out.println("a = " + a);
+        System.out.println("b = " + b);
+
+        System.out.println("Equal (==): " + (a == b));
+        System.out.println("Not Equal (!=): " + (a != b));
+    }
+}
+
+/* output
+  a = 10,
+b = 20,
+ false,
+  true
+  */
