2a990a5732107b893d6115edf58249c55cdbf85e
naveencheery4-creator <naveencheery4@gmail.com>
2026-06-12 15:03:30 +0530

    Create LogicalOperatorsDemo.java

---
+++ Operators/LogicalOperatorsDemo.java
@@ -0,0 +1,17 @@
+public class LogicalOperatorsDemo {
+
+    public static void main(String[] args) {
+
+        boolean condition1 = true;
+        boolean condition2 = false;
+
+        // Logical AND
+        System.out.println("AND (&&): " + (condition1 && condition2));
+
+        // Logical OR
+        System.out.println("OR (||): " + (condition1 || condition2));
+
+        // Logical NOT
+        System.out.println("NOT (!): " + (!condition1));
+    }
+}
