e1c5d434ae6bed15a61411143b1951014f478412
naveencheery4-creator <naveencheery4@gmail.com>
2026-06-12 14:56:38 +0530

    Create FunctionPrintName.java

---
+++ Java-Basics/FunctionPrintName.java
@@ -0,0 +1,16 @@
+public class FunctionPrintName {
+
+    // Function to print name
+    public void printName() {
+        System.out.println("Naveen Kumar");
+    }
+
+    public static void main(String[] args) {
+
+        // Create object
+        FunctionPrintName obj = new FunctionPrintName();
+
+        // Call function
+        obj.printName();
+    }
+}
