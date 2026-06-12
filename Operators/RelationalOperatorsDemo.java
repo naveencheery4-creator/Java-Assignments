7bcbb13bde4f1baebbd72fde87fdaac9c66b635c
naveencheery4-creator <naveencheery4@gmail.com>
2026-06-12 15:04:19 +0530

    Create RelationalOperatorsDemo.java

---
+++ Operators/RelationalOperatorsDemo.java
@@ -0,0 +1,16 @@
+public class RelationalOperatorsDemo {
+
+    public static void main(String[] args) {
+
+        int a = 20;
+        int b = 10;
+
+        System.out.println("a = " + a);
+        System.out.println("b = " + b);
+
+        System.out.println("a > b  : " + (a > b));
+        System.out.println("a < b  : " + (a < b));
+        System.out.println("a >= b : " + (a >= b));
+        System.out.println("a <= b : " + (a <= b));
+    }
+}
