c6df93a28a84e95a1166f075da4c1bc62780ed86
naveencheery4-creator <naveencheery4@gmail.com>
2026-06-12 15:02:43 +0530

    Create TwoNumbersEqualOrNot.java

---
+++ Operators/TwoNumbersEqualOrNot.java
@@ -0,0 +1,14 @@
+public class TwoNumbersEqualOrNot {
+
+    public static void main(String[] args) {
+
+        int num1 = 25;
+        int num2 = 25;
+
+        if (num1 == num2) {
+            System.out.println("Both numbers are equal.");
+        } else {
+            System.out.println("Both numbers are not equal.");
+        }
+    }
+}
