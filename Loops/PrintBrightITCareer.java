c490fe559442f4311160ddb235c5afe93485bfb8
naveencheery4-creator <naveencheery4@gmail.com>
2026-06-12 15:32:20 +0530

    Create PrintBrightITCareer.java

---
+++ Loops/PrintBrightITCareer.java
@@ -0,0 +1,22 @@
+public class PrintBrightITCareer {
+
+    public static void main(String[] args) {
+
+        for (int i = 1; i <= 10; i++) {
+            System.out.println("Bright IT Career");
+        }
+    }
+}
+
+/* output
+  Bright IT Career
+Bright IT Career
+Bright IT Career
+Bright IT Career
+Bright IT Career
+Bright IT Career
+Bright IT Career
+Bright IT Career
+Bright IT Career
+Bright IT Career
+  */
