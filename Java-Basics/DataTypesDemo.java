ada08cee5b5e453255528d4b38bd5ac009de5ccc
naveencheery4-creator <naveencheery4@gmail.com>
2026-06-12 14:51:01 +0530

    Create DataTypesDemo.java

---
+++ Java-Basics/DataTypesDemo.java
@@ -0,0 +1,17 @@
+public class DataTypesDemo {
+
+    public static void main(String[] args) {
+
+        int age = 22;
+        boolean isJavaLearner = true;
+        char grade = 'A';
+        float percentage = 85.5f;
+        double salary = 25000.75;
+
+        System.out.println("Age: " + age);
+        System.out.println("Java Learner: " + isJavaLearner);
+        System.out.println("Grade: " + grade);
+        System.out.println("Percentage: " + percentage);
+        System.out.println("Salary: " + salary);
+    }
+}
