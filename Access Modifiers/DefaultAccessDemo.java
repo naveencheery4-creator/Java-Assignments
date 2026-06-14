ee92916c9ad919fbe5b362fd7a5551655c8ed006
naveencheery4-creator <naveencheery4@gmail.com>
2026-06-14 12:22:05 +0530

    Create DefaultAccessDemo.java

---
+++ Access Modifiers/DefaultAccessDemo.java
@@ -0,0 +1,16 @@
+public class DefaultAccessDemo {
+
+    public static void main(String[] args) {
+
+        DefaultClass obj = new DefaultClass();
+
+        System.out.println("Number: " + obj.number);
+
+        obj.display();
+    }
+}
+
+/* Output
+  Number: 100
+Default Method Called
+  */
