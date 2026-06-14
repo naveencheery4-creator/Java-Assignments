2ae5f9a3c88156f24006441c0ad638b784a30b6f
naveencheery4-creator <naveencheery4@gmail.com>
2026-06-14 12:00:13 +0530

    Update A.java

--- Inheritance/Inheritance/A.java
+++ Inheritance/Inheritance/A.java
@@ -1,1 +1,14 @@
+public class A {
 
+    public void methodA1() {
+        System.out.println("Method A1");
+    }
+
+    public void methodA2() {
+        System.out.println("Method A2");
+    }
+
+    public void display() {
+        System.out.println("Display Method in Class A");
+    }
+}
