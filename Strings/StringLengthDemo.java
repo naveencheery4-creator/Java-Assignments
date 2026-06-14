7e2ba11edafc7978df2975c5a92decc345f82ce2
naveencheery4-creator <naveencheery4@gmail.com>
2026-06-14 11:36:29 +0530

    Create StringLengthDemo.java

---
+++ Strings/StringLengthDemo.java
@@ -0,0 +1,17 @@
+public class StringLengthDemo {
+
+    public static void main(String[] args) {
+
+        String text = "Java Programming";
+
+        int length = text.length();
+
+        System.out.println("String: " + text);
+        System.out.println("Length: " + length);
+    }
+}
+
+/* Output 
+  String: Java Programming
+Length: 16
+  */
