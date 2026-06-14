420743fbf8b7d7643f7b63bb49acab5cddfd5763
naveencheery4-creator <naveencheery4@gmail.com>
2026-06-14 12:12:36 +0530

    Create ConstructorDemo.java

---
+++ Inheritance/ConstructorDemo.java
@@ -0,0 +1,16 @@
+public class ConstructorDemo {
+
+    public static void main(String[] args) {
+
+        Child obj = new Child(10, 20, 30, 40);
+    }
+}
+
+/* Output
+  Parent Constructor
+a = 10
+b = 20
+c = 30
+d = 40
+Child Constructor
+  */
