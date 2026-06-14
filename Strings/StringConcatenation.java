02824b78b7788213e12cebc8d9a7322adc31923b
naveencheery4-creator <naveencheery4@gmail.com>
2026-06-14 11:34:55 +0530

    Create StringConcatenation.java

---
+++ Strings/StringConcatenation.java
@@ -0,0 +1,16 @@
+public class StringConcatenation {
+
+    public static void main(String[] args) {
+
+        String firstName = "Naveen";
+        String lastName = "Kumar";
+
+        String fullName = firstName + " " + lastName;
+
+        System.out.println("Full Name: " + fullName);
+    }
+}
+
+/* Output
+  Full Name: Naveen Kumar
+  */
