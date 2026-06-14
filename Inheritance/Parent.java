6a89a772e6e326af5fe7fee833042e12a164781f
naveencheery4-creator <naveencheery4@gmail.com>
2026-06-14 12:10:49 +0530

    Create Parent.java

---
+++ Inheritance/Parent.java
@@ -0,0 +1,11 @@
+public class Parent {
+
+    Parent(int a, int b, int c, int d) {
+
+        System.out.println("Parent Constructor");
+        System.out.println("a = " + a);
+        System.out.println("b = " + b);
+        System.out.println("c = " + c);
+        System.out.println("d = " + d);
+    }
+}
