a0257340f2b65a81cdf294b166c93363f5927747
naveencheery4-creator <naveencheery4@gmail.com>
2026-06-14 12:04:59 +0530

    Create C.java

---
+++ Inheritance/C.java
@@ -0,0 +1,15 @@
+public class C extends B {
+
+    public void methodC1() {
+        System.out.println("Method C1");
+    }
+
+    public void methodC2() {
+        System.out.println("Method C2");
+    }
+
+    @Override
+    public void display() {
+        System.out.println("Display Method in Class C");
+    }
+}
