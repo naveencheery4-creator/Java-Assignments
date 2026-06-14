ef6223748b0875af3bc0ba7d6934b8a36fcb785f
naveencheery4-creator <naveencheery4@gmail.com>
2026-06-14 12:16:20 +0530

    Create DataMemberDemo.java

---
+++ Inheritance/DataMemberDemo.java
@@ -0,0 +1,13 @@
+public class DataMemberDemo {
+
+    public static void main(String[] args) {
+
+        ParentData obj = new ChildData();
+
+        System.out.println(obj.number);
+    }
+}
+
+/* Output
+  100
+  */
